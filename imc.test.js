fe857008f80dfb6286e45d5333f8f78a3c71f5a0
egdsilva <edinaldo.gaspar@philips.com>
2026-05-09 09:55:21 -0300

    fix testes

--- imc.test.js
+++ imc.test.js
@@ -11,7 +11,3 @@ test('IMC de 90kg e 1.80m deve ser aproximadamente 27.78', () => {
 test('IMC de 50kg e 1.60m deve ser aproximadamente 19.53', () => {
     expect(calcularIMC(50, 1.60)).toBeCloseTo(19.53, 1);
 });
-
-test('IMC de 80kg e 1.70m deve ser 10 (teste de falha)', () => {
-    expect(calcularIMC(80, 1.70)).toBe(10);
-});
